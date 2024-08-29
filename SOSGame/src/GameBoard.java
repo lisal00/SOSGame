d321ccbdd5bb4b9e8124e06600e11c5e2e3faab2
lisal00 <143776518+lisal00@users.noreply.github.com>
2024-08-29 17:19:34 -0500

    line

--- SOSGame/src/GameBoard.java
+++ SOSGame/src/GameBoard.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GameBoard extends JFrame{
-    private static final int CELL_SIZE = 100;
+public class GameBoard extends JPanel{
+    private static final int CELL_SIZE = 10;
     private int gridWidth;
     private int gridHeight;
     int leftMargin;
@@ -10,13 +10,14 @@ public class GameBoard extends JFrame{
 
     public GameBoard(){
         setBackground(Color.WHITE);
+        //setVisible(true);
     }
 
     public void paint(Graphics g){
         super.paint(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.fillRect(10, 10, 100, 100);
+//        g2.fillRect(10, 10, 80, 80);
 
         int width = getWidth();
         int height = getHeight();
@@ -27,11 +28,23 @@ public class GameBoard extends JFrame{
         int xSpare = width - (gridWidth * CELL_SIZE);
         int ySpare = height - (gridWidth * CELL_SIZE);
 
-        leftMargin = xSpare / 2;
-        topMargin = ySpare / 2;
+        leftMargin = xSpare / 3;
+        topMargin = ySpare / 3;
 
-        g2.setColor(Color.WHITE);
+        g2.setColor(Color.RED);
         g2.fillRect(leftMargin, topMargin, width-xSpare, height - ySpare);
+        g2.setColor(Color.BLACK);
+        g2.drawLine(40,90,500,90);
+//
+//        g2.setColor(Color.BLACK);
+//        for(int gridy = 0; gridy < gridHeight; gridy++){
+//            for(int gridx = 0; gridy < gridWidth; gridx++){
+//                int x = gridx * CELL_SIZE + leftMargin;
+//                int y = gridy * CELL_SIZE + topMargin;
+//
+//                g2.fillRect(x, y, CELL_SIZE -1 , CELL_SIZE - 1);
+//            }
+//        }
 
     }
 
