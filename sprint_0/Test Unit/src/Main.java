14ce176d6c44eddd8adf4e15bc20adbdd3a12c3a
lisal00 <143776518+lisal00@users.noreply.github.com>
2024-09-04 22:54:03 -0500

    Add files via upload

---
+++ sprint_0/Test Unit/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public String addString(String A, String B){
+        return A + B;
+    }
+
+    public int addInt(int A, int B){
+        return A + B;
+    }
+}
