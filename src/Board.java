3d19f0a784dc092f027f5f1a33ee63eabae422f6
lisal00 <143776518+lisal00@users.noreply.github.com>
2024-09-29 23:10:30 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,37 @@
 public class Board {
     private int[][] grid;
+    private char turn = 'B';
+    int maxVal = 5;
 
-    public Board(int size){
+    public Board(){
+        grid = new int[maxVal][maxVal];
+    }
+
+    public int getCell(int row, int column) {
+        if (row >= 0 && row < maxVal && column >= 0 && column < maxVal)
+            return grid[row][column];
+        else
+            return -1;
+    }
+
+    public void updateGrid(int size){
         grid = new int[size][size];
+        maxVal = size;
     }
 
-    public void getBoard(){
-
+    public int getGridSize(){
+        return maxVal;
     }
 
-    public void setBoard(){
-
+    public char getTurn(){
+        return turn;
     }
+
+    public void makeMove(int row, int column) {
+        if (row >= 0 && row < maxVal && column >= 0 && column < maxVal && grid[row][column] == 0) {
+            grid[row][column] = (turn == 'B')? 1 : 2;
+            turn = (turn == 'B')? 'R' : 'B';
+        }
+    }
+
 }
