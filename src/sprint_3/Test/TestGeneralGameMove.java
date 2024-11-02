169da2f12a175e0e0d764a53784968475fb47a01
lisal00 <143776518+lisal00@users.noreply.github.com>
2024-11-01 23:36:01 -0500

    Add files via upload

---
+++ src/sprint_3/Test/TestGeneralGameMove.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestGeneralGameMove {
+    private Board board;
+    int row = 4;
+    int col = 1;
+
+    @Before
+    public void setUp() throws Exception{
+        board = new GeneralGame();
+        board.setGameMode(Board.gameMode.GENERAL);
+    }
+
+    @Test //AC 6.1
+    public void testMakingSMoveGeneral(){
+        board.setBlueMoveChoice(Board.Cell.S);
+        board.makeMove(row, col);
+        assertEquals(Board.Cell.S, board.getCell(row,col));
+        assertEquals('R', board.getTurn());
+    }
+
+    @Test //AC 6.2
+    public void testMakingOMoveGeneral(){
+        board.setBlueMoveChoice(Board.Cell.O);
+        board.makeMove(row, col);
+        assertEquals(Board.Cell.O, board.getCell(row,col));
+        assertEquals('R', board.getTurn());
+    }
+
+    @Test //AC 6.3
+    public void testGeneralOccupiedCell() {
+        board.setBlueMoveChoice(Board.Cell.S);
+        board.setRedMoveChoice(Board.Cell.O);
+
+        board.makeMove(row,col);
+        board.makeMove(row,col);
+        assertEquals(Board.Cell.S, board.getCell(row,col));
+        assertEquals('R', board.getTurn());
+    }
+}
